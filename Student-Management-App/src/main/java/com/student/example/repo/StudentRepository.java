f57ae7d96b9f45728e1d747c35afe60536f5e27f
Sanket Bondre <118126570+sanketbondre01@users.noreply.github.com>
2025-08-20 17:31:35 +0530

    Delete Student-Management-App directory

--- Student-Management-App/src/main/java/com/student/example/repo/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.student.example.repo;
-
-import com.student.example.entities.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
