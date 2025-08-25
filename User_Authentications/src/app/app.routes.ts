4fc3d6354de367294b9222a13e119f6c90fa39b8
Sanket Bondre <118126570+sanketbondre01@users.noreply.github.com>
2025-08-25 23:13:56 +0530

    Add files via upload

---
+++ User_Authentications/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'signup', pathMatch: 'full' },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent }
+];
